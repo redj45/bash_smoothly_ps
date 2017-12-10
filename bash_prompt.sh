6bc8057f2192e4bfa3fdd98375e316c8204f25f5
Serg Kondrashov <12582799+sergkondr@users.noreply.github.com>
2017-12-10 19:02:45 +0300

    new mc color scheme

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -1,33 +1,31 @@
 function __git__branch {
-        git branch 2> /dev/null| sed -e '/^[^*]/d' -e "s/* \(.*\)/(\1$(__git_status)) /" 
+    git branch 2> /dev/null| sed -e '/^[^*]/d' -e "s/* \(.*\)/(\1$(__git_status)) /"
 }
 
 function __git_status {
-        [[ $(LC_ALL=C git status 2> /dev/null | tail -n1) != *"nothing to commit"* ]] && echo " [*]"
+    [[ $(LC_ALL=C git status 2> /dev/null | tail -n1) != *"nothing to commit"* ]] && echo " [*]"
 }
 
 function __virtual_env {
-        if [[ $VIRTUAL_ENV != "" ]] ; then 
-		            echo env:$(basename $VIRTUAL_ENV)\ 
-	      fi 
+    [[ $VIRTUAL_ENV != "" ]] && echo env:$(basename $VIRTUAL_ENV)" "
 }
 
 if [[ ${EUID} == 0 ]] ; then
-	PS1='\
+    PS1='\
 \[\033[0;31m\]┌[ \[\033[1;31m\]\H\[\033[1;34m\]: \w \[\033[0;31m\]]\[\033[0m\]
-\[\033[0;31m\]└[ \[\033[0m\]\[\033[1;31m\]$(__virtual_env)$(__git__branch)\[\033[0;31m\]\[\033[0m\]\[\033[1;31m\]\u\[\033[0;31m\] ]-> \[\033[0m\]'
+\[\033[0;31m\]└[ \[\033[0m\]\[\033[1;31m\]$(__virtual_env)$(__git__branch)\[\033[0;31m\]\[\033[0m\]\[\033[1;31m\]\u\[\033[0;31m\] ]-> \[\033[0m\]';
 else
-        PS1='\
+    PS1='\
 \[\033[0;32m\]┌[ \[\033[1;32m\]\H\[\033[1;34m\]: \w \[\033[0;32m\]]\[\033[0m\]
 \[\033[0;32m\]└[ \[\033[0m\]\[\033[1;31m\]$(__virtual_env)$(__git__branch)\[\033[0;31m\]\[\033[0m\]\[\033[1;32m\]\u\[\033[0;32m\] ]-> \[\033[0m\]';
-       
 fi
 
 alias ls='ls --color';
 alias ll='ls -la';
 alias rm='rm -i';
 alias mv='mv -i';
-alias mc='mc -b';
+alias mc='mc --colors normal=green,default:selected=brightmagenta,gray:marked=yellow,default:markselect=yellow,gray:directory=blue,default:executable=brightgreen,default:link=cyan,default:device=brightmagenta,default:special=lightgray,default:errors=red,default:reverse=green,default:gauge=green,default:input=white,gray:dnormal=green,gray:dfocus=brightgreen,gray:dhotnormal=cyan,gray:dhotfocus=brightcyan,gray:menu=green,default:menuhot=cyan,default:menusel=green,gray:menuhotsel=cyan,default:helpnormal=cyan,default:editnormal=green,default:editbold=blue,default:editmarked=gray,blue:stalelink=red,default';
+alias cls='clear';
 
 unset use_color safe_term match_lhs
 export HISTCONTROL=ignoreboth
